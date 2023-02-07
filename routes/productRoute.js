6b64e5dd11b6e2a2107c44edafe3901e9577fc71
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2023-02-07 10:34:29 -0800

    worked on the produts

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const { createProduct } = require('../controllers/productController')
+const { createProduct, getProducts, getProduct,deleteProduct, updateProduct } = require('../controllers/productController')
 const protect = require('../middleWare/authMiddleware')
 const{upload} =require('../utils/fileUpload')
 const router = express.Router()
 
  router.post('/',protect,upload.single("image"),createProduct)
-
+ router.patch('/:id',protect,upload.single("image"),updateProduct)
+ router.get('/',protect,getProducts)
+ router.get('/:id',protect,getProduct)
+ router.delete('/:id',protect,deleteProduct)
+ 
  module.exports =router
